e81c6ba35775a6a960af76fd6dbf69dcbb8e9025
aminmemonn <125481954+aminmemonn@users.noreply.github.com>
2025-11-22 15:53:29 -0600

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.tool-card').forEach(card => {
+    card.addEventListener('click', function(e) {
+        e.preventDefault();
+        const tool = this.getAttribute('data-tool');
+    });
+});
+
+document.getElementById('moreToolsBtn').addEventListener('click', function(e) {
+    e.preventDefault();;
+});
+
+document.querySelectorAll('#navBar a').forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.innerWidth < 768) {
+            document.getElementById('navBar').classList.remove('active');
+        }
+    });
+});
