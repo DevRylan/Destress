0af2331cbe1a843cf79ce972aa0220fa52866638
DevRylan <everydaynormalvlogs@gmail.com>
2025-12-10 22:14:25 -0600

    Change database path to /tmp/destress.db

--- deactivateUser.php
+++ deactivateUser.php
@@ -4,8 +4,9 @@ if (!isset($_SESSION["adminId"])) exit;
 
 $id = $_POST["id"] ?? "";
 
-$conn = new PDO("sqlite:destress.db");
+$conn = new PDO("sqlite:/tmp/destress.db");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $conn->prepare("DELETE FROM Users WHERE userId=?")->execute([$id]);
 ?>
+
